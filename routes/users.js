48fd715a989d26ead480f7e2a980142a226a2877
beforeIkillYou <simonsimple305@gmail.com>
2024-01-07 22:50:12 +0530

    partiallly compted the insta clone

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require('mongoose')
+const plm = require('passport-local-mongoose');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
-module.exports = router;
+mongoose.connect('mongodb+srv://root:root@cluster0.g5ociou.mongodb.net/');
+
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  password: String,
+  profileImage: String,
+  bio: String,
+  
+  posts : [{type: mongoose.Schema.Types.ObjectId , ref: 'post'}]
+})
+
+userSchema.plugin(plm);
+
+module.exports = mongoose.model('user' , userSchema);
